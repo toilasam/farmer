11e1f3e8a242ea2ec194804d2ab797538ad38438
lean <xuanlvph52216@gmail.com>
2024-09-26 15:14:15 +0700

    main.py
    Signed-off-by: lean <xuanlvph52216@gmail.com>

--- main.py
+++ main.py
@@ -1,40 +1,43 @@
-
-from books import Books
-from book_loan_requests import Book_loan_requests, Request_manager
-from categories import Categories
-from customers import Customers
-from publishers import Publishers
+from books import *
+from book_loan_requests import *
+from categories import *
+from customers import *
+from publishers import *
+from confirm import confirm_exit
 
 class_library = {
-    "1": Books,
-    "2": Book_loan_requests,
-    "3": Categories,
-    "4": Customers,
-    "5": Publishers
+    "1": 'books',
+    "2": 'requests',
+    "3": 'categories',
+    "4": 'customers',
+    "5": 'publishers'
 }
 
 if __name__ == "__main__":
     while True:
-        print('-----------Chương Trình Quản Lý Thư Viện------------')
+        print('-------------Chương Trình Quản Lý Thư Viện------------')
+        Request_manager.notifications_late_loans(Request_manager.check_loans())
         print(''' 
             1: Books          4: Customers
             2: Requests       5: Publishers
             3: Categories  ''')
-        user_input = input("Nhập mã đối tượng muốn tương tác ('exit' để thoát): ")
-        if user_input == 'exit':
-            print("Bye")
-            break
-
-        if user_input in class_library:
-            if user_input == '1': #### ví dụ nhập 1 để tương tác với đối tượng sách:
+        user_input = input("Nhập mã đối tượng muốn tương tác ('q' để thoát): ").strip().lower()
+        if user_input == 'q':
+            confirm_exit()
+            
+        elif user_input in class_library or user_input in list(class_library.values()): 
+            if user_input == '1' or user_input == 'books': 
                 print('Books')
-                pass
-            if user_input == '2':#### ví dụ nhập 2 để tương tác với đối tượng phiếu mượn sách:
+                Book_manager.choose_action()
+            elif user_input == '2' or user_input == 'requests':
                 print('Requests')
                 Request_manager.choose_action()
-            ##### mọi người điền tiếp ÌF để tương tác với phần bài của mọi người dưới đây ####
+            elif user_input == '3':
+                pass
+            elif user_input == '4':
+                pass
+            elif user_input == '5':
+                pass
 
         else:
             print("Đối tượng không tồn tại. Vui lòng thử lại.")
-
-    print('Thoát chương trình.')
