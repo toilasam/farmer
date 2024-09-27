cf0cb868ab509f4c1bdd34c736b1d4ef9ca939ce
lean <xuanlvph52216@gmail.com>
2024-09-28 00:50:53 +0700

    main.py
    Signed-off-by: lean <xuanlvph52216@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from books import *
 from book_loan_requests import Request_manager
 from categories import *
-from customers import *
+
 from publishers import *
 from confirm import confirm_exit
-from Doc_Gia import *
+from Doc_gia import *
+import cate
 
 class_library = {
     "1": 'books',
@@ -15,31 +16,38 @@ class_library = {
 }
 
 if __name__ == "__main__":
+    ds_doc_gia = []  # Khởi tạo danh sách độc giả
+    # Bạn có thể khởi tạo một số độc giả mẫu nếu cần
+    doc_gia1 = DocGia(ma='001', ten='John Doe', email='john@example.com', sdt='123456789')
+    ds_doc_gia.append(doc_gia1)
+
+
     while True:
         print('-------------Chương Trình Quản Lý Thư Viện------------')
         Request_manager.notifications_late_loans(Request_manager.check_loans())
         print(''' 
-            1: Books          4: Customers
-            2: Requests       5: Publishers
-            3: Categories  ''')
+            1: Sách             4: Độc giả
+            2: Phiếu mượn       5: Nhà xuất bản
+            3: Thể loại  ''')
         user_input = input("Nhập mã đối tượng muốn tương tác ('q' để thoát): ").strip().lower()
         if user_input == 'q':
             confirm_exit()
             
-        elif user_input in class_library or user_input in list(class_library.values()): 
+        elif user_input in class_library: # or user_input in list(class_library.values()): 
             if user_input == '1' or user_input == 'books': 
-                print('Books')
+                print('Sách')
                 Book_manager.choose_action()
             elif user_input == '2' or user_input == 'requests':
-                print('Requests')
+                print('Phiếu mượn')
                 Request_manager.choose_action()
             elif user_input == '3': 
-                pass
+                cate.menu_quan_ly()
+                # pass
             elif user_input == '4':
-                DocGia.chon_chuc_nang()
+                doc_gia = DocGia(ma='001', ten='John Doe', email='john@example.com', sdt='123456789')
+                doc_gia.chon_chuc_nang(ds_doc_gia)  # Gọi phương thức từ đối tượng và truyền ds_doc_gia
             elif user_input == '5':
                 Publishers.menu()
-                pass
         else:
             print("Đối tượng không tồn tại. Vui lòng chọn lại.")
 
