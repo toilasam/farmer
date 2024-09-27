be512b5b5b307fa86c270caaab3b7aa3268cb644
lean <xuanlvph52216@gmail.com>
2024-09-28 00:41:04 +0700

    Add files via upload
    Signed-off-by: lean <xuanlvph52216@gmail.com>

---
+++ customers.py
@@ -0,0 +1,24 @@
+class Customers:
+    def __init__(self, customer_ID, first_name, last_name, phone, address, mail, registration_date):
+        self.customer_ID = customer_ID
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone = phone
+        self.address = address
+        self.mail = mail
+        self.registration_date = registration_date
+
+    def add_new_customers(self):
+        pass
+
+    def update_customer(self):
+        pass
+    
+    def show_customers(self):
+        pass
+
+    def seach_customers(self):
+        pass
+
+    def del_customers(self):
+        pass
